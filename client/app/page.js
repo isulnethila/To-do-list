63620649dd9e1dc80a0313227e75b0d56d845513
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-06 23:32:45 +0530

    Task page design

--- client/app/page.js
+++ client/app/page.js
@@ -1,17 +1,42 @@
 import Image from "next/image";
 import { Poppins } from 'next/font/google'
 import Sidbar from "./components/sidbar";
+import { MdDeleteOutline } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
+import { MdCheck } from "react-icons/md";
+
 
 const poppins = Poppins({
-  weight: ['100','200','400','600'],
+  weight: ['100', '200', '400', '600'],
   subsets: ['latin'],
 })
 
 export default function Home() {
   return (
     <main className={`${poppins.className} p-5`} >
-     
-     
+      <div className="mx-10 md:mx-40 lg:mx-70 ">
+        <div className=" bg-gray-500 p-5 grid grid-cols-2 rounded-2xl">
+          <div className="flex flex-col">
+            <label>
+              title1
+            </label>
+            <label>
+              description
+            </label>
+          </div>
+          <div className="flex gap-2 md:gap-4 lg:gap-7">
+            <button className=" cursor-pointer hover:text-gray-700"><MdDeleteOutline/></button>
+            <button className=" cursor-pointer hover:text-gray-700"><CiEdit/></button>
+            <button className=" cursor-pointer hover:text-gray-700"><MdCheck/></button>
+
+
+          </div>
+        </div>
+
+        
+
+      </div>
+
     </main>
   );
 }
