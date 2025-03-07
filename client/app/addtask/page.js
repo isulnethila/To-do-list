c03a421e8f9282520154bc4b5ff41814aa7b5b88
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-07 12:04:22 +0530

    data pass addtask to home

--- client/app/addtask/page.js
+++ client/app/addtask/page.js
@@ -1,28 +1,56 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
+import { useRouter } from "next/navigation";
 
 export default function page() {
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+    const [date,setDate]=useState("");
+
+    const router=useRouter();
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        router.push(`/?title=${title}&description=${description}&date=${date}`);
+    }
+  
+    
     return (
         <div className="mx-20 md:mx-40 lg:mx-70">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-5">
-                    <label for="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
-                    <input type="title"  id="title" className="bg-gray-50 border border-gray-300  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="title" required />
+                    <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
+                    <input  type="text"  
+                        id="title"
+                        name="title"
+                        value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                    className="bg-gray-50 border border-gray-300  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="title" required />
                 </div>
 
                 <div className="mb-5">
-                <label for="title" className="block mb-2 text-sm font-medium text-gray-900">Date</label>
+                <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900">Date</label>
                 <input type="Date"
-                
+                 id="date"
+                 name="date"
+                 value={date}
+                onChange={(e)=>setDate(e.target.value)}
                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@flowbite.com" required />
                 </div>
 
                 <div className="">
-                <label for="message" className="block mb-2 text-sm font-medium text-gray-900">Description</label>
-                <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Write your thoughts here..."></textarea>
+                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900">Description</label>
+                <textarea  id="description"
+                        name="description"
+                        value={description}
+                onChange={(e)=>setDescription(e.target.value)}
+                rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Write your thoughts here..."></textarea>
                 </div>
 
                 <div>
-                    <button className="p-4 w-28 mt-5 text-white text-xl hover:bg-gray-600 bg-gray-500 rounded">Add</button>
+                    <button type="submit" className="p-4 w-28 mt-5 text-white text-xl hover:bg-gray-600 bg-gray-500 rounded">Add</button>
                 </div>
 
             </form>
--- client/app/page.js
+++ client/app/page.js
@@ -1,10 +1,11 @@
+"use client";
 import Image from "next/image";
 import { Poppins } from 'next/font/google'
 import Sidbar from "./components/sidbar";
 import { MdDeleteOutline } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
 import { MdCheck } from "react-icons/md";
-
+import { useSearchParams } from "next/navigation";
 
 const poppins = Poppins({
   weight: ['100', '200', '400', '600'],
@@ -12,16 +13,26 @@ const poppins = Poppins({
 })
 
 export default function Home() {
+  const searchParams=useSearchParams();
+
+  const title=searchParams.get("title");
+  const description=searchParams.get("description");
+  const date=searchParams.get("date");
+  
   return (
+    
     <main className={`${poppins.className} p-5`} >
       <div className="mx-10 md:mx-40 lg:mx-70 ">
         <div className=" bg-gray-500 p-5 grid grid-cols-2 rounded-2xl border border-gray-800 focus:ring-blue-500 focus:border-gray-500 ">
           <div className="flex flex-col">
             <label className="font-bold">
-              title1
+              {title}
             </label>
             <label>
-              description
+              {date}
+            </label>
+            <label>
+              {description}
             </label>
           </div>
           <div className="flex gap-2 md:gap-4 lg:gap-7">
